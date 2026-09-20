fe3bd13161cea9ee581e6f64fcbf47c8aa477012
okehamptontabletennisclub-tech <okehamptontabletennisclub@gmail.com>
2026-09-20 16:46:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -278,8 +278,6 @@ function createSessionBlock(session) {
         block.classList.add('smb');
     } else if (name.includes('tournament') || name.includes('edttl') || name.includes('tte')) {
         block.classList.add('tournament');
-    } else if (name.includes('wren music')) || name.includes('wrenmusic ')) {
-        block.classList.add('wren-music');
         {
     
     const sessionType = session.sessionType.toLowerCase();
